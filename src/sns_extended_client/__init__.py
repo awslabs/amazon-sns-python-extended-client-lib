693023ce655b9d7f62d31d082b4dd751af76c0f0
Mit Suthar <mitvs@amazon.com>
2025-02-03 11:16:08 -0800

    Fix the compatibility issue with boto3

--- src/sns_extended_client/__init__.py
+++ src/sns_extended_client/__init__.py
@@ -2,8 +2,7 @@ import boto3
 
 from .session import SNSExtendedClientSession
 
-# Monkey patch to use our Session object instead of boto3's
-boto3.session.Session = SNSExtendedClientSession
+setattr(boto3.session, "Session", SNSExtendedClientSession)
 
 # Now take care of the reference in the boto3.__init__ module
 setattr(boto3, "Session", SNSExtendedClientSession)
